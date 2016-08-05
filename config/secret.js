b49b881f6d2603421fc14e9dec16bba54e213993
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2016-08-05 15:29:14 -0500

    modify callbackURL

--- config/secret.js
+++ config/secret.js
@@ -7,6 +7,6 @@ module.exports = {
         clientID: process.env.FACEBOOK_ID || '1757809527841344',
         clientSecret: process.env.FACEBOOK_SECRET || 'ba36ff15fe7a45f11b727692dcc8ef27',
         profileFields: ['email', 'displayName'],
-        callbackURL: 'http://process.env.PORT/auth/facebook/callback'
+        callbackURL: 'http://'+process.env.PORT+'/auth/facebook/callback'
     }
 };
