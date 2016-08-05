b370452494d8124b80b57a33d80fe1319f4fba3d
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2016-08-05 15:46:30 -0500

    commit all files

--- config/secret.js
+++ config/secret.js
@@ -5,8 +5,8 @@ module.exports = {
 
     facebook: {
         clientID: process.env.FACEBOOK_ID || '1757809527841344',
-        clientSecret: process.env.FACEBOOK_SECRET || 'ba36ff15fe7a45f11b727692dcc8ef27',
+        clientSecret: process.env.FACEBOOK_SECRET || '',
         profileFields: ['email', 'displayName'],
-        callbackURL: 'http://'+process.env.PORT+'/auth/facebook/callback'
+        callbackURL: 'http://localhost:3000/auth/facebook/callback'
     }
 };
