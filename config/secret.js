9f631f54f54ce1798e4b7cb67c02c518976e41e0
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2016-08-05 15:25:11 -0500

    modify port

--- config/secret.js
+++ config/secret.js
@@ -1,12 +1,12 @@
 module.exports = {
     database: 'mongodb://Deng:13919273153dtc@ds031925.mlab.com:31925/ecommerce_deng',
-    port: 3000,
+    port: process.env.PORT,
     secretKey: "TingchangDeng@Zhu",
 
     facebook: {
         clientID: process.env.FACEBOOK_ID || '1757809527841344',
         clientSecret: process.env.FACEBOOK_SECRET || 'ba36ff15fe7a45f11b727692dcc8ef27',
         profileFields: ['email', 'displayName'],
-        callbackURL: 'http://localhost:3000/auth/facebook/callback'
+        callbackURL: 'http://process.env.PORT/auth/facebook/callback'
     }
 };
